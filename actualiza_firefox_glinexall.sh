3ba1ff61e9613531900e95e7da363125fb3cc8da
homero10 <azamrod@gmail.com>
2018-04-10 18:13:16 +0200

    Cambios de implementación
    Al subir el .desktop directamente a GitHub ya no es necesario hacer previamente un cambio de formato a .png (como si había que hacer en WordPress) para después volver a pasarlo a .desktop (acceso directo para GNU/Linux) por lo que se comentan los procesos en donde se realizaban dichos cambios.
    
    Cambio de URL a la hora de obtener el .desktop.

--- actualiza_firefox_glinexall.sh
+++ actualiza_firefox_glinexall.sh
@@ -7,7 +7,7 @@
 HOME=~/
 DESCARGA=https://download-installer.cdn.mozilla.net/pub/firefox/releases/59.0.2/linux-i686/es-ES/firefox-59.0.2.tar.bz2
 DIRECTORIO=firefox*.tar.bz2
-LANZADOR=https://usandoguadalinexedu.files.wordpress.com/2018/04/firefox-noroot.png
+LANZADOR=https://github.com/homero10/actualiza-firefox-guadalinex/blob/master/firefox-noroot.desktop
 NEWLANZADOR=firefox-noroot.desktop
 ROJO="\033[1;31m"
 NORMAL="\033[0m"
@@ -39,11 +39,11 @@ echo -e "${AZUL}Firefox se está descomprimiendo en su directorio personal...${N
 
 pv $DIRECTORIO | tar -xjf - -C $HOME
 
-#Descargamos el lanzador del nuevo Firefox, de momento alojado en usandoguadalinexedu.
+#Descargamos el lanzador del nuevo Firefox, de momento alojado en usandoguadalinexedu. TODO: ahora alojado en github, no hace falta conversión de .png a .desktop
 
 wget $LANZADOR -q
 
-#Se cambia formato de archivo para poder hacerlo ejecutable y se copia a las rutas correspondientes.
+#Se cambia formato de archivo para poder hacerlo ejecutable y se copia a las rutas correspondientes. TODO: Ya no es necesario, se comenta.
 
 echo -e "${AZUL}Creando rutas y lanzadores...${NORMAL}"
 
@@ -51,7 +51,7 @@ echo -e "${AZUL}Creando rutas y lanzadores...${NORMAL}"
 
 mkdir ~/.local/share/applications 2> /dev/null
 
-mv firefox-noroot.png $NEWLANZADOR
+#mv firefox-noroot.png $NEWLANZADOR
 
 cp $NEWLANZADOR ~/.local/share/applications/
 
